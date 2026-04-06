ffc5a7ea0ba3753eeb84d3f44e233c380d1a1b7e
sahuaoaoao <adityasworld562@gmail.com>
2026-04-06 14:55:16 +0530

    Initial commit UC1

---
+++ OOPSBannerApp.java
@@ -0,0 +1,16 @@
+/**
+ * OOPSBannerApp - UC1
+ * This program prints "OOPS" to the console.
+ * 
+ * @author Supran
+ * @version 1.0
+ */
+public class OOPSBannerApp {
+
+    /**
+     * Main method - Entry point of the application
+     */
+    public static void main(String[] args) {
+        System.out.println("OOPS");
+    }
+}
